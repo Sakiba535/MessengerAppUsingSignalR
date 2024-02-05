cf78c10ab92a9f454bd4c1006caf44f32352edf8
RAKIBA1998 <146844851+RAKIBA1998@users.noreply.github.com>
2024-02-05 18:54:59 +0600

    Add project files.

---
+++ MessengerApp/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerApp.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task Share(string user, string message)
+        {
+           // await Clients.All.SendAsync("receive",user,message);
+            await Clients.Caller.SendAsync("receive", "(SELF)", message);
+
+            await Clients.Others.SendAsync("receive", user, message);
+        }
+    }
+}
---
+++ MessengerApp/wwwroot/js/chat.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../lib/microsoft/signalr/dist/browser/signalr.js" />
+
+
+var username=prompt("enter Your name", "DITC");
+
+var hubBuilder = new signalR.HubConnectionBuilder();
+var connection = hubBuilder.withUrl("/chat").build();
+
+connection.on("receive", function (user, message) {
+    var li = document.createElement("li");
+    li.textContent = `${user} : ${message}`;
+    $('#chatLog').append(li);
+
+
+});
+
+connection.start();
+$('#btnSend').on('click', function () {
+
+    var message = $('#txtInput').val();
+    connection.invoke("Share", username, message);
+    $('#txtInput').val(null);
+
+})
+
